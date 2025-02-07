04faf6cd84588c64a4962454bc6c0517d371cf1f
Axel Lois <axel9034@hotmail.com>
2025-02-06 23:43:38 -0300

    some more env variables changes

--- api/src/db/database.ts
+++ api/src/db/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async (): Promise<void> => {
   await mongoose.connect(
-    `${process.env.MONGO_URL}/${process.env.MONGO_DB_NAME}`
+    `${process.env.MONGO_URL}`
   );
   console.log("db is connected.");
 };
